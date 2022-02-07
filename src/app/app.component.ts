0183544a3c8091a936de77c8a67b6e94b2619a40
Eren Baş <62754751+ErennBas@users.noreply.github.com>
2022-02-07 20:09:41 +0300

    calls and status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonSegment, IonSlides } from '@ionic/angular';
+import { StatusBar } from '@awesome-cordova-plugins/status-bar/ngx';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
+  providers: [ StatusBar ]
 })
 export class AppComponent {
   @ViewChild('slider', { read: undefined, static: false }) slider: IonSlides;
@@ -15,7 +17,11 @@ export class AppComponent {
     speed: 100
   };
 
-  constructor() {}
+  constructor(private statusBar: StatusBar) {
+    console.log('STATUS BAR ===>>>>', this.statusBar);
+    this.statusBar.overlaysWebView(true);
+    this.statusBar.backgroundColorByHexString('#00a884');
+  }
 
   async segmentChanged(event: any) {
     const slideId = +(event.detail.value as string).replace('ion-sb-', '');
--- src/app/components/calls/calls.component.ts
+++ src/app/components/calls/calls.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-calls',
@@ -6,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calls.component.scss'],
 })
 export class CallsComponent implements OnInit {
+  profileCdn: string = environment.profilePhotoCdn;
+  contacts = [
+    {
+      contactName: 'Finn',
+      contactAvatar: this.profileCdn + '1.jfif',
+      snippet: 'Listen, I\'ve had a pretty messed up day...',
+      time: new Date(Date.now())
+    },
+    {
+      contactName: 'Han',
+      contactAvatar: this.profileCdn + '2.jfif',
+      snippet: 'I\'ve got enough on my plate as it is, and I...',
+      time: new Date(Date.now())
+    },
+    {
+      contactName: 'Rey',
+      contactAvatar: this.profileCdn + '4.jfif',
+      snippet: 'You will remove these restraints and leave...',
+      time: new Date(Date.now())
+    },
+    {
+      contactName: 'Luke',
+      contactAvatar: this.profileCdn + '5.jfif',
+      snippet: 'I feel the good in you, the conflict...',
+      time: new Date(Date.now())
+    }
+  ];
 
   constructor() { }
 
